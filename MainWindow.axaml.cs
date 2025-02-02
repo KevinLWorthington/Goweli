6ad8daf98252a8df52697417e99ad68a9eb3da52
Kevin Worthington <kevinlworthington@gmail.com>
2025-02-02 10:42:34 -0500

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Goweli
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
